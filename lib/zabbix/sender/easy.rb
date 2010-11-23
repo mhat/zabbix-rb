541a18df58d1cb5df9d05eefb2d99c27c28ed6ec
Matthew Knopp <mknopp@yammer-inc.com>
2010-11-22 20:13:46 -0800

    remove artifact

--- lib/zabbix/sender/easy.rb
+++ lib/zabbix/sender/easy.rb
@@ -1,6 +1,6 @@
 class Zabbix::Sender::Easy
   def self.run (task, opts={}, &block)
-    zbx = Zabbix::Sender::Easy.new(opts)
+    zbx = Zabbix::Sender.new(opts)
     zbx.send_start 
     block.call(zbx)
     zbx.send_stop 
